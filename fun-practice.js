d14a7dae32d291fb914b10109b07179265f291ce
Naumanhassan307 <Naumanhassan307@gmail.com>
2020-12-29 12:51:17 -0800

    complete mul fun

--- fun-practice.js
+++ fun-practice.js
@@ -1,1 +1,9 @@
-// practice of functions
+    // practice of functions
+
+
+    function mul(v1, v2){
+        const result=(v1 * v2);
+        return console.log(`value ${v1} multiply by value ${v2} is : ${result}`);
+    }
+    mul(5,2);
+
