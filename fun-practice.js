c014747ff9898b36e9e8cf8dab58950bb87b4747
Naumanhassan307 <Naumanhassan307@gmail.com>
2020-12-30 15:43:39 -0800

    complete basic function

--- fun-practice.js
+++ fun-practice.js
@@ -7,3 +7,10 @@
     }
     mul(5,2);
 
+    //  fun
+    function basic(v1, v2){
+        const result=(v1 + v2) *v2;
+        return console.log(`value ${v1} multiply by value ${v2} is : ${result}`);
+    }
+    mul(5,2);
+
